94930fd52738acbb59f5fb9320f9631b5601d1c2
Atif-Kazmi <atifkazmi1980@gmail.com>
2024-11-07 10:11:07 +0500

    Create expense_tracker.py

---
+++ scripts/expense_tracker.py
@@ -0,0 +1,9 @@
+# scripts/expense_tracker.py
+
+class ExpenseTracker:
+    def __init__(self, expenses):
+        self.expenses = expenses
+
+    def update_expenses(self):
+        # For example purposes, update expenses if necessary
+        pass
